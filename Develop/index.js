077187369249e8b1db800864baec8d7fde1565a5
PatMAllen <116239708+PatMAllen@users.noreply.github.com>
2023-01-04 23:40:53 -0500

    Challenge 9

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,10 @@
 // TODO: Include packages needed for this application
 const inquirer = require ('inquirer');
 const fs = require ('fs');
+const util = require('util');
+
+const generateMarkdown = require('./utils/generateMarkdown');
+
 console.log('Are you ready to Create a REM-GEN 1');
 // TODO: Create an array of questions for user input
 const questions = [
@@ -8,63 +12,84 @@ const questions = [
       type: 'input',
       message: 'What is your Project title?',
       name: 'title',
-      validate: (value)=>{if(value){return true} else {return 'I need a value to continue'}},
+    },
+    {
+      type:'input',
+      message: 'What is this project about? Please give an description?',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Table of Contents.',
+      name:'Table of Contents'
     },
     {
       type: 'input',
       message: 'How do you Install your app?',
       name: 'installation',
-      validate: (value)=>{if(value){return true} else {return 'I need a value to continue'}},
+      
     },
     {
       type: 'input',
-      message: 'Are there any Instruction that to be followed?',
+      message: 'Are there any Instruction that need to be followed?',
       name: 'instructions',
-      validate: (value)=>{if(value){return true} else {return 'I need a value to continue'}},
-    },
+      
+    }, 
     {
       type: 'input',
-      message: 'The amount of Credits?',
-      name: 'installation',
-      validate: (value)=>{if(value){return true} else {return 'I need a value to continue'}},
-    },
-    {
-      type: 'input',
-      message: 'How do you use your app?',
+      message: 'How is your app used? Give instructions',
       name: 'usage',
-      validate: (value)=>{if(value){return true} else {return 'I need a value to continue'}},
+      
     },
     {
       type: 'List',
       message: 'What liscense did you need?',
       name: 'license',
-      choices: ['The MIT License', 'The GPL Liscense', 'Apache Liscense', 'N/A']
-      validate: (value)=>{if(value){return true} else {return 'I need a value to continue'}},
+      choices:'licenses'      
     },
     {
       type: 'input',
-      message: 'GitHub username:',
+      message: 'What is your GitHub username:',
       name: 'git',
-      validate: (value)=>{if(value){return true} else {return 'I need a value to continue'}},
+      
     },
     {
       type: 'input',
-      message: 'E-mails',
+      message: 'What is your E-mails',
       name: 'email',
-      validate: (value)=>{if(value){return true} else {return 'I need a value to continue'}},
+     
+    },
+    {
+      type: 'input',
+      message: 'Who contributed to this project',
+      name: 'contributing',
     }
+
 ]
 
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+      console.log(fileName)
+      console.log(data)
+      if(err) {
+        return console.log(err)
+      } else {
+        console.log('success')
+      }
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() { 
+  inquirer.prompt(questions)
+  .then(function(data) {
+    writeToFile('README.md', generateMarkdown(data));
+    console.log(data)
+  })
 
-     return inquirer.prompt(questions);
 }
-
 // Function call to initialize app
 init();
